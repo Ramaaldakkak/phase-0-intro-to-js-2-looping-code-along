0aa6d321750c17bdf958564c3e6629b55d2e46f2
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-05-06 07:31:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const names = ["Bruce","James","John"];
+const event = "birthday";
+ 
+function writeCards(names, event) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+       message.push("Thank you, " + names[i]+ "," + " for the wonderful " + event + " gift!");
+    }
+    return message;
+}
+ 
+writeCards(names, event);
+ 
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer --;
+    }
+    console.log(integer);
+}
+ 
+countDown(10);
